502bc8ff5f85c2efcedf8cbe2b6b4c17c509b127
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-08-18 15:41:32 -0300

    Barra de pesquisa de usuário com botão de convidar.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.css';
-
+import api from '../services/api';
+import InviteIcon from '../assets/account-plus.svg'
 export default function Home() {
     
-
+    const [searchUsername, setSearchUsername] = useState('');
     const [menuOpened, setMenuOpened] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [searchResult, setSearchResult] = useState([]);
+    
+    async function GetRegisteredUser() {
+        const response = await api.get('/users');
+        setUsers(response.data);
+    }
+    
+    useEffect(() => {
+        GetRegisteredUser();
+    }, []);
+    useEffect(handleUserSearch, [searchUsername]);
     
     function handleMenuClick() {
         setMenuOpened(!menuOpened);
+    }
+    
+    function handleUserSearch() {
+        if(searchUsername.trim()  === '') {
+            setSearchResult([]);
+            return;
+        }
+        setSearchResult(users.filter(user => user.username.includes(searchUsername.trim())));
+    }
+    
+    async function handleInvite(targetId) {
+        console.log(targetId);
     }
     
     return (
@@ -21,7 +46,22 @@ export default function Home() {
                 </div>
             </header>
             { menuOpened ? <div className="menu">
-                
+                <input placeholder="Usuário" value={searchUsername} onChange={e => setSearchUsername(e.target.value)}/>
+                <div className="search-result-container">
+                    {searchResult.length > 0 ?
+                        (<ul>
+                            {searchResult.map(result => (
+                                <li key={result._id} className="search-result-item">
+                                    <p>{result.username}</p>
+                                    <img src={InviteIcon} alt="Convidar" onClick={() => {
+                                        handleInvite(result._id);
+                                    }}/>
+                                </li>
+                            ))}
+                        </ul>)
+                        : null}
+                </div>
+                    
             </div> : null}
         </div>
     );
