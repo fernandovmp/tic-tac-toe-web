528014bcbf3943f3c91b9d8ec0aadf04a4e5b53e
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-08-19 18:27:23 -0300

    LogIn/SignIn com autenticação e autoriazação com web tokens.

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,25 +1,63 @@
 import React, { useState } from 'react';
 import './Login.css';
+import api from '../services/api';
+import Cookies from 'universal-cookie';
 
-export default function Login() {
+export default function Login({ history }) {
     
+    const cookies = new Cookies();
     const [createAccount, setCreateAccount] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     
-    async function handleLogin() {
-        if(createAccount) {
-            console.log('Create account');
-            return;
+    async function handleLogin(e) {
+        e.preventDefault();
+        try {
+            if (createAccount) {
+
+                if (password !== confirmPassword) {
+                    throw new Error('passwords doesn\'t match');
+                }
+                const { userAlreadyExists } = await api.post('/users', {
+                    username,
+                    password
+                });
+                if(userAlreadyExists) {
+                    throw new Error('user already exists');
+                }
+            }
+            const response = await api.post('/login', {
+                username,
+                password
+            });
+            if(response.status === 401) {
+                throw new Error('bad credentials');
+            }
+            cookies.set('access-token', response.headers['set-cookie']);
+            history.push('/');
+        } catch (error) {
+            
         }
-        console.log('loggin');
+        
     }
     
     return (
         <div className="login-container">
             <form onSubmit={handleLogin}>    
-                <input placeholder="Nome de usuario"/>
-                <input placeholder="Senha" type="password" />
+                <input placeholder="Nome de usuario"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <input placeholder="Senha" type="password" 
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
                 {createAccount && (
-                    <input placeholder="Confirmar senha" type="password" />
+                    <input placeholder="Confirmar senha" type="password" 
+                        value={confirmPassword}
+                        onChange={e => setConfirmPassword(e.target.value)}
+                    />
                 )
                 }
                 <button type="submit">Entrar</button>
