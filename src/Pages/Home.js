222f239eb5ad87231b886770957613238a3f1bbf
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-08-24 19:17:57 -0300

    Inicia partida ao aceitar convite e funções para realizar o jogo.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,6 +7,28 @@ import NotificationIcon from '../assets/bell-outline.svg';
 import AcceptInvite from '../assets/email.svg';
 import io from 'socket.io-client';
 
+const board = ['', '', '', '', '', '', '', '', ''];
+const symbols = ['X', 'O'];
+const winningSequences = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+const defaultGameState = {
+    board,
+    currentSymbol: -1,
+    players: [],
+    matchState: {
+        end: false,
+        result: ''
+    }
+}
+
 export default function Home({ history }) {
     
     const [loggedUser, setLoggedUser] = useState({});
@@ -17,19 +39,13 @@ export default function Home({ history }) {
     const [invites, setInvites] = useState([]);
     const [auth, setAuth] = useState(true);
     const [notificationCount, setNotificationCount] = useState(0);
-    const board = ['', '', '', '', '', '', '', '', ''];
-    const symbols = ['X', 'O'];
+    const [socket, setSocket] = useState(null);
+    const [gameState, setGameState] = useState(defaultGameState);
     
     async function getLoggedUser() {
         try {
             const response = await api.get('/login/user');
             setLoggedUser(response.data);
-            const socket = io('http://localhost:3001', {
-                query: {
-                    user: response.data._id
-                }
-            });
-            socket.on('invite', () => GetInvites());
         } catch (error) {
             setAuth(false);
         }
@@ -66,6 +82,19 @@ export default function Home({ history }) {
     
     useEffect(handleUserSearch, [searchUsername]);
     
+    useEffect(() => {
+        const localSocket = io('http://localhost:3001', {
+            query: {
+                user: loggedUser._id
+            }
+        });
+        localSocket.on('invite', () => GetInvites());
+        localSocket.on('startPlay', startState => {
+            setGameState(startState);
+            localSocket.on('makePlay', handleMakePlay);
+        });
+        setSocket(localSocket);
+    }, [loggedUser])
     
     useEffect(() => {
         let cont = 0;
@@ -103,6 +132,81 @@ export default function Home({ history }) {
         });
     }
     
+    async function handleMakePlay(newState) {
+        setGameState(newState);
+        if (newState.matchState.end) {
+            let requestData = {};
+            if (newState.matchState.result === loggedUser._id) {
+                requestData = {
+                    wonMatches: loggedUser.wonMatches + 1
+                };
+            }
+            else if (newState.matchState.result === 'tied') {
+                requestData = {
+                    tiedMatches: loggedUser.tiedMatches + 1
+                };
+            }
+            else {
+                requestData = {
+                    lostMatches: loggedUser.lostMatches + 1
+                };
+            }
+            await api.patch(`/users/${loggedUser._id}`, requestData);
+            setGameState(defaultGameState);
+        }
+        
+    }
+    
+    function checkWinningSequence(symbol) {
+        for(let i in winningSequences) {
+            if (gameState.board[winningSequences[i][0]] === symbol &&
+                gameState.board[winningSequences[i][1]] === symbol &&
+                gameState.board[winningSequences[i][2]] === symbol) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    async function startPlay(opponentUser) {
+        const defaultState = {
+            board,
+            currentSymbol: 0,
+            players: [opponentUser, loggedUser._id],
+            matchState: {
+                end: false,
+                result: ''
+            }
+        };
+        setGameState(defaultState);
+        socket.emit('startPlay', defaultState);
+        socket.on('makePlay', handleMakePlay);
+    }
+    
+    async function handlePlay(index) {
+        if(gameState.players[gameState.currentSymbol] !== loggedUser._id) {
+            return;
+        }
+        if(gameState.board[index] !== '') {
+            return;
+        }
+        gameState.board[index] = symbols[gameState.currentSymbol];
+        if(checkWinningSequence(symbols[gameState.currentSymbol])) {
+            gameState.matchState = {
+                end: true,
+                result: symbols[gameState.currentSymbol]
+            }
+        }
+        if(gameState.board.indexOf('') === -1) {
+            gameState.matchState = {
+                end: true,
+                result: 'tied'
+            }
+        }
+        gameState.currentSymbol = gameState.currentSymbol === 0 ? 1 : 0;
+        socket.emit('makePlay', gameState);
+    }
+    
     return (
         <div className="page-container">
             {!auth && <Redirect to="/login" /> }
@@ -125,7 +229,9 @@ export default function Home({ history }) {
                                 <strong className="invite-header">Você recebeu um convite!</strong>
                                 <div>
                                     <p>{invite.sender.username} convidou você para uma partida!</p>
-                                    <button><img src={AcceptInvite} alt="Aceitar convite"/></button>
+                                    <button onClick={() => startPlay(invite.sender._id)}>
+                                        <img src={AcceptInvite} alt="Aceitar convite"/>
+                                    </button>
                                 </div>
                             </div>
                             ))}
@@ -167,32 +273,34 @@ export default function Home({ history }) {
                 </div>
                 <div className="game">
                     <div className="board">
-                        <div className="board-cell">
-                            <div>{board[0]}</div>
+                        <div className="board-cell" onClick={() => handlePlay(0) }>
+                            <div>{gameState.board[0]}</div>
                         </div>
-                        <div className="board-cell board-vertical-border">
-                            <div>{board[1]}</div>
+                        <div className="board-cell board-vertical-border" onClick={() => handlePlay(1)}>
+                            <div>{gameState.board[1]}</div>
                         </div>
-                        <div className="board-cell">
-                            <div>{board[2]}</div>
+                        <div className="board-cell" onClick={() => handlePlay(2)}>
+                            <div>{gameState.board[2]}</div>
                         </div>
-                        <div className="board-cell board-horizontal-border">
-                            <div>{board[3]}</div>
+                        <div className="board-cell board-horizontal-border" onClick={() => handlePlay(3)}>
+                            <div>{gameState.board[3]}</div>
                         </div>
-                        <div className="board-cell board-vertical-border board-horizontal-border">
-                            <div>{board[4]}</div>
+                        <div className="board-cell board-vertical-border board-horizontal-border" 
+                            onClick={() => handlePlay(4)}
+                        >
+                            <div>{gameState.board[4]}</div>
                         </div>
-                        <div className="board-cell board-horizontal-border">
-                            <div>{board[5]}</div>
+                        <div className="board-cell board-horizontal-border" onClick={() => handlePlay(5)}>
+                            <div>{gameState.board[5]}</div>
                         </div>
-                        <div className="board-cell">
-                            <div>{board[6]}</div>
+                        <div className="board-cell" onClick={() => handlePlay(6)}>
+                            <div>{gameState.board[6]}</div>
                         </div>
-                        <div className="board-cell board-vertical-border">
-                            <div>{board[7]}</div>
+                        <div className="board-cell board-vertical-border" onClick={() => handlePlay(7)}>
+                            <div>{gameState.board[7]}</div>
                         </div>
-                        <div className="board-cell">
-                            <div>{board[8]}</div>
+                        <div className="board-cell" onClick={() => handlePlay(8)}>
+                            <div>{gameState.board[8]}</div>
                         </div>
                     </div>
                 </div>
